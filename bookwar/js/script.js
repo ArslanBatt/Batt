993d9d53566df6f4234ca709ed7e62198fa5441d
ArslanBatt <148847954+ArslanBatt@users.noreply.github.com>
2023-12-11 17:55:20 +0500

    90%

--- bookwar/js/script.js
+++ bookwar/js/script.js
@@ -15,3 +15,21 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
 }
+
+
+
+let btn = document.getElementById("Show-modal");
+btn.addEventListener("click", showModal);
+
+function showModal(){
+  let modal = document.getElementsByClassName("model-login")[0];
+  modal.style.top = "30%"
+}
+
+let btn2 = document.getElementById("Show-modal2");
+btn2.addEventListener("click", showModal2);
+
+function showModal2(){
+  let modal2 = document.getElementsByClassName("model-reg")[0];
+  modal2.style.top = "30%"
+}
